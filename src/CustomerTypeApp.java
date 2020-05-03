81b9ad78fa6e97373f2cf6bc0b933db22389bfbf
Boris Moiseev <bmoiseev@digital-spirit.ru>
2020-05-04 02:39:22 +0400

    Initial commit

---
+++ src/CustomerTypeApp.java
@@ -0,0 +1,10 @@
+public class CustomerTypeApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Добро пожаловать в тестовое приложение \"Тип покупателя\"\n");
+
+    }
+
+    // метод, который принимает перечисление CustomerType
+}
